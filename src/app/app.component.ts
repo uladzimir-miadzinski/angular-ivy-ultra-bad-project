7acc6dd6e82dd43b39b7b55d06eb01612c47e654
uladzimir-miadzinski <webmedinsky@gmail.com>
2022-02-08 14:34:54 +0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { UpperCasePipe } from '@angular/common';
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 import { LoggerService } from './services/logger.service';
 
 @Component({
