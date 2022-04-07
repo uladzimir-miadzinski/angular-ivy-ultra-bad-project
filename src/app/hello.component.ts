b8efe07bc744c9126e47b80e3bfcf3cc28e19002
uladzimir-miadzinski <webmedinsky@gmail.com>
2022-04-07 14:53:07 +0300

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1 class="display-4">Hello<br>{{name}}!</h1>`,
+  template: `<h1 class="display-4">Hello,<br>{{name}}!</h1>`,
   styles: [`h1 { text-align: center }`]
 })
 export class HelloComponent  {
