ad08650c90c55009881558618370e9819a4dfe1c
uladzimir-miadzinski <webmedinsky@gmail.com>
2022-04-07 14:24:28 +0300

    1 file modified

--- src/app/services/toolbar.service.ts
+++ src/app/services/toolbar.service.ts
@@ -1,5 +1,15 @@
+import { LoggerService } from "./logger.service"
+
 export class ToolbarService {
+  logger: LoggerService;
+
+  constructor() {
+    this.logger = new LoggerService();
+  }
+
   getToolbar() {
+    this.logger.log('Returned toolbar config');
+    
     return [
       {
         label: 'Home',
